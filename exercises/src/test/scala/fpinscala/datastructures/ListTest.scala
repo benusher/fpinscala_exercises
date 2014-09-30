5da5aa422044b69ce33d64d7f2b13a18f5dd77f1
Ben Usher <ben_usher@hotmail.com>
2014-09-30 21:19:52 +0100

    Implement List.tail

---
+++ exercises/src/test/scala/fpinscala/datastructures/ListTest.scala
@@ -0,0 +1,15 @@
+package fpinscala.datastructures
+
+import org.specs2.mutable.Specification
+
+class ListTest extends Specification {
+
+  "A list" should {
+    "can return the tail of a list" in {
+      List.tail(List(1, 2, 3)) must beEqualTo(List(2, 3))
+    }
+    "tail is Nil if list is Nil" in {
+      List.tail(Nil) must beEqualTo(Nil)
+    }
+  }
+}
